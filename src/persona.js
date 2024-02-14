36eebe19e74568442c689ace870c95a9a08acc41
Heidi Negrete <heidi@negrete.dev>
2024-02-13 20:19:47 -0500

    initial commit

---
+++ src/persona.js
@@ -0,0 +1,54 @@
+export const persona = {
+    firstName: "Ben",
+    secondName: "Schwartz",
+    description: "Stormcare Specialist",
+    email: "ben.s@northeastroofingsolutions.com",
+    story:
+      "I'm a dude who does things with roofs that you should pay to do things with your roof in particular.",
+  sections: {
+    // You can create your own sections. But! For reducing headache in future, change the values (content), but don't change the keys ('title', 'desc').
+    books: [
+        {
+            title: "Example Service",
+            desc: 'not expensive, saves your roofs from aging',
+            url: 'https://www.amazon.com/Shop-Class-Soulcraft-Inquiry-Value/dp/0143117467?keywords=soulcraft&qid=1700379052&sr=8-3'
+        },
+        {
+            title: "Reach for the Skies",
+            desc: 'expensive, beautifies vain roofs',
+            url: 'https://www.amazon.com/Why-We-Drive-Toward-Philosophy/dp/0062741977?crid=2Z7ID7AXG1M8P&keywords=why+we+drive&qid=1700379191&sprefix=why+we+dri,aps,410&sr=8-1'
+        },
+        {
+            title: "The Tinkering Roof",
+            desc: 'for philosophical roofs',
+            url: 'https://www.amazon.com/Tinkering-Mind-Agency-Cognition-Extended-ebook/dp/B0BDZZGJRS?crid=2EJ2CHS3HAB8F&keywords=tinkering+mind&qid=1700379284&s=books&sprefix=tinkering+mi,stripbooks-intl-ship,296&sr=1-1'
+        },
+     ]}};
+//     experience: [
+//         {
+//             title: "Indian Motorcycles",
+//             desc: '2010-now',
+//             url: 'https://www.indianmotorcycle.com/en-us/'
+//         },
+//         {
+//             title: "Harley Davidson",
+//             desc: '1999-2010',
+//             url: 'https://www.harley-davidson.com/us/en/index.html'
+//         },
+//     ],
+//   },
+//       contacts: [
+//       {
+//         title: "x",
+//         url: "https://twitter.com/0x_shapeshifter",
+//       },
+//       {
+//         title: "mastodon",
+//         url: "https://mastodon.social/@0xshapeshifter",
+//       },
+//     ],
+
+//   };
+
+    // Don't forget to save upon changing the contents of the file ;)
+
